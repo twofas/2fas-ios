8fb2c02621c9ae96bf1a579d34c22b11579b272a
Zbigniew Cisiński <zbigniew@cisinski.com>
2023-08-01 14:58:12 +0200

    TF-1213 PoC for different sheet height

--- TwoFAS/TwoFAS/Root/RootViewController.swift
+++ TwoFAS/TwoFAS/Root/RootViewController.swift
@@ -18,6 +18,7 @@
 //
 
 import UIKit
+import SwiftUI
 
 final class RootViewController: ContainerViewController {
     override func viewDidLoad() {
@@ -30,5 +31,134 @@ final class RootViewController: ContainerViewController {
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
         super.viewWillTransition(to: size, with: coordinator)
         NotificationCenter.default.post(Notification(name: Notification.Name.orientationSizeWillChange))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        let modal = ModalViewController()
+//        modal.modalTransitionStyle = .coverVertical
+//        modal.modalPresentationStyle = .overCurrentContext
+//        modal.definesPresentationContext = true
+//        present(modal, animated: true)
+        
+        let test = TestViewController()
+        test.update = { value, vc in
+            if let sheet = vc.sheetPresentationController {
+                UIView.animate(withDuration: 0.3, animations: {
+                    test.mainView?.alpha = 0
+                }, completion: { val in
+                    print(val)
+                    sheet.animateChanges {
+                        sheet.detents = [.custom(resolver: { context in
+                            min(value, context.maximumDetentValue)
+                        })]
+                    }
+                    UIView.animate(withDuration: 0.3, delay: 0, animations: {
+                        test.mainView?.alpha = 1
+                    })
+                })
+                
+            }
+        }
+        if let sheet = test.sheetPresentationController {
+            sheet.detents = [.custom(resolver: { context in
+                400.0
+            })]
+//                sheet.largestUndimmedDetentIdentifier = .medium
+                sheet.prefersScrollingExpandsWhenScrolledToEdge = false
+                sheet.prefersEdgeAttachedInCompactHeight = true
+//                sheet.widthFollowsPreferredContentSizeWhenEdgeAttached = true
+            }
+            present(test, animated: true, completion: nil)
+    }
+}
+
+
+final class TestViewController: UIViewController {
+    var update: ((CGFloat, UIViewController) -> Void)?
+    var mainView: UIView?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        let v = V(change: {
+            self.update?($0, self)
+        })
+        let vc = UIHostingController(rootView: v)
+        
+        vc.willMove(toParent: self)
+        addChild(vc)
+        view.addSubview(vc.view)
+        vc.view.pinToParent()
+        vc.didMove(toParent: self)
+        mainView = vc.view
+//        let b = UIButton()
+//        b.setTitle("Change", for: .normal)
+//        b.setTitleColor(.white, for: .normal)
+//        b.backgroundColor = .white
+//        b.addTarget(self, action: #selector(change), for: .touchUpInside)
+//        view.addSubview(b, with: [
+//            b.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//            b.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+//        ])
+    }
+    
+//    @objc
+//    func change() {
+//        update?(self)
+//    }
+}
+
+struct V: View {
+    let change: (CGFloat) -> Void
+  
+    @State var currentHeight: CGFloat = 400
+    @State var username: String = "Test"
+    
+    @State private var height = CGFloat.zero
+    
+    var body: some View {
+            GeometryReader { geo in
+                VStack(alignment: .center, spacing: 10) {
+                    Button("Change") {
+                        currentHeight = CGFloat((100...600).randomElement() ?? 300)
+                    }
+                    .frame(height: currentHeight)
+                    TextField(
+                        "User name (email address)",
+                        text: $username
+                    )
+                    .foregroundColor(.white)
+                    .background(
+                        Rectangle()
+                            .fill(.blue)
+                    )
+                }
+                .background(GeometryReader {
+                                    // store half of current width (which is screen-wide)
+                                    // in preference
+                                    Color.clear
+                                        .preference(key: ViewHeightKey.self,
+                                            value: $0.frame(in: .local).size.height)
+                                })
+                                .onPreferenceChange(ViewHeightKey.self) {
+                                    // read value from preference in state
+                                    self.height = $0
+                                    change(height)
+                                }
+//                .onChange(of: geo.size.height) { newValue in
+//                    print(">>> \(newValue)")
+//                    change(newValue)
+//                }
+            }
+    }
+}
+
+struct ViewHeightKey: PreferenceKey {
+    typealias Value = CGFloat
+    static var defaultValue = CGFloat.zero
+    static func reduce(value: inout Value, nextValue: () -> Value) {
+        value += nextValue()
     }
 }

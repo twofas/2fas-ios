73e9ffecd5e4faf2feac61a84719187fcb1555ab
Zbigniew Cisiński <zbigniew@cisinski.com>
2023-09-21 22:13:31 +0200

    TF-1375 Token window placement fix

--- TwoFAS/TwoFAS/Root/Modules/AddingService/Token/View/AddingServiceTokenView.swift
+++ TwoFAS/TwoFAS/Root/Modules/AddingService/Token/View/AddingServiceTokenView.swift
@@ -39,75 +39,22 @@ struct AddingServiceTokenView: View {
                     AddingServiceCloseButtonView {
                         dismiss()
                     }
-                    AddingServiceTitleView(text: T.Tokens.addSuccessTitle)
-                }
-                .frame(maxWidth: .infinity)
-                AddingServiceTextContentView(text: T.Tokens.addSuccessDescription)
-                AddingServiceLargeSpacing()
-                
-                VStack(spacing: 6) {
-                    HStack(spacing: 12) {
-                        Image(uiImage: presenter.serviceIcon)
-                            .accessibilityHidden(true)
-                        
-                        VStack(spacing: 4) {
-                            AddingServiceTitleView(text: presenter.serviceName, alignToLeading: true)
-                                .lineLimit(1)
-                            if let serviceAdditionalInfo = presenter.serviceAdditionalInfo {
-                                AddingServiceAdditionalInfoView(text: serviceAdditionalInfo)
-                            }
+                    .frame(maxWidth: .infinity)
+                    HStack {
+                        Spacer()
+                            .frame(maxWidth: .infinity)
+                        VStack(alignment: .center, spacing: Theme.Metrics.standardSpacing) {
+                            AddingServiceTitleView(text: T.Tokens.addSuccessTitle)
+                            AddingServiceTextContentView(text: T.Tokens.addSuccessDescription)
+                            AddingServiceLargeSpacing()
+                            tokenView()
                         }
+                        .frame(minWidth: 310, alignment: .center)
+                        Spacer()
+                            .frame(maxWidth: .infinity)
                     }
-                    HStack(alignment: .center, spacing: 2 * ThemeMetrics.spacing) {
-                        AddingServiceTokenValueView(text: $presenter.token, willChangeSoon: $presenter.willChangeSoon)
-                        
-                        switch presenter.serviceTokenType {
-                        case .totp:
-                            ZStack(alignment: .center) {
-                                AddingServiceTOTPTimerView(
-                                    text: $presenter.time,
-                                    willChangeSoon: $presenter.willChangeSoon
-                                )
-                                
-                                Circle()
-                                    .trim(from: 0, to: $animationProgress.animation(animation).wrappedValue)
-                                    .stroke(Color(presenter.willChangeSoon ? ThemeColor.theme : ThemeColor.primary),
-                                            style: StrokeStyle(
-                                                lineWidth: 1,
-                                                lineCap: .round
-                                            ))
-                                    .rotationEffect(.degrees(-90))
-                                    .padding(0.5)
-                                    .frame(width: 30, height: 30)
-                            }
-                        case .hotp:
-                            Button {
-                                    withAnimation(
-                                        .linear(duration: 1)
-                                        .speed(5)
-                                        .repeatCount(1, autoreverses: false)
-                                    ) {
-                                        rotationAngle = 360.0
-                                    }
-                            } label: {
-                                Asset.refreshTokenCounter.swiftUIImage
-                                    .tint(Color(presenter.refreshTokenLocked ? ThemeColor.inactive : ThemeColor.theme))
-                                    .rotationEffect(.degrees(rotationAngle))
-                            }
-                            .disabled(presenter.refreshTokenLocked)
-                            .onAnimationCompleted(for: rotationAngle) {
-                                rotationAngle = 0
-                                presenter.handleRefresh()
-                            }
-                        }
-                    }
+                    .frame(maxWidth: .infinity)
                 }
-                .padding(.init(top: 16, leading: 20, bottom: 12, trailing: 20))
-                .frame(maxWidth: .infinity)
-                .overlay(
-                    RoundedRectangle(cornerRadius: Theme.Metrics.modalCornerRadius)
-                        .stroke(Color(Theme.Colors.Line.selectionBorder), lineWidth: 1)
-                )
             }
             .padding(.horizontal, Theme.Metrics.doubleMargin)
             
@@ -129,5 +76,71 @@ struct AddingServiceTokenView: View {
                 animationProgress = newValue
             }
         }
+    }
+    
+    @ViewBuilder
+    private func tokenView() -> some View {
+        VStack(spacing: 6) {
+            HStack(spacing: 12) {
+                Image(uiImage: presenter.serviceIcon)
+                    .accessibilityHidden(true)
+                
+                VStack(spacing: 4) {
+                    AddingServiceTitleView(text: presenter.serviceName, alignToLeading: true)
+                        .lineLimit(1)
+                    if let serviceAdditionalInfo = presenter.serviceAdditionalInfo {
+                        AddingServiceAdditionalInfoView(text: serviceAdditionalInfo)
+                    }
+                }
+            }
+            HStack(alignment: .center, spacing: 2 * ThemeMetrics.spacing) {
+                AddingServiceTokenValueView(text: $presenter.token, willChangeSoon: $presenter.willChangeSoon)
+                
+                switch presenter.serviceTokenType {
+                case .totp:
+                    ZStack(alignment: .center) {
+                        AddingServiceTOTPTimerView(
+                            text: $presenter.time,
+                            willChangeSoon: $presenter.willChangeSoon
+                        )
+                        
+                        Circle()
+                            .trim(from: 0, to: $animationProgress.animation(animation).wrappedValue)
+                            .stroke(Color(presenter.willChangeSoon ? ThemeColor.theme : ThemeColor.primary),
+                                    style: StrokeStyle(
+                                        lineWidth: 1,
+                                        lineCap: .round
+                                    ))
+                            .rotationEffect(.degrees(-90))
+                            .padding(0.5)
+                            .frame(width: 30, height: 30)
+                    }
+                case .hotp:
+                    Button {
+                            withAnimation(
+                                .linear(duration: 1)
+                                .speed(5)
+                                .repeatCount(1, autoreverses: false)
+                            ) {
+                                rotationAngle = 360.0
+                            }
+                    } label: {
+                        Asset.refreshTokenCounter.swiftUIImage
+                            .tint(Color(presenter.refreshTokenLocked ? ThemeColor.inactive : ThemeColor.theme))
+                            .rotationEffect(.degrees(rotationAngle))
+                    }
+                    .disabled(presenter.refreshTokenLocked)
+                    .onAnimationCompleted(for: rotationAngle) {
+                        rotationAngle = 0
+                        presenter.handleRefresh()
+                    }
+                }
+            }
+        }
+        .padding(.init(top: 16, leading: 20, bottom: 12, trailing: 20))
+        .overlay(
+            RoundedRectangle(cornerRadius: Theme.Metrics.modalCornerRadius)
+                .stroke(Color(Theme.Colors.Line.selectionBorder), lineWidth: 1)
+        )
     }
 }

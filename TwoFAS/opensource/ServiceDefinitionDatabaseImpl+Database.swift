9224af6e5a230ca9ee169f39d15623527b3e6a0d
Zbigniew Cisiński <zbigniew@cisinski.com>
2024-01-21 23:51:06 +0100

    v5.2.5 Correct placeholders for OS implementation

--- TwoFAS/opensource/ServiceDefinitionDatabaseImpl+Database.swift
+++ TwoFAS/opensource/ServiceDefinitionDatabaseImpl+Database.swift
@@ -20,8 +20,157 @@
 import UIKit
 import Common
 
-// swiftlint:disable all
-final class ServiceDefinitionDatabaseGenerated {
-    lazy var services: [ServiceDefinition] = {[]}()
+public protocol ServiceDefinitionDatabase: AnyObject {
+    func listAll() -> [ServiceDefinition]
+    func service(using serviceTypeID: ServiceTypeID) -> ServiceDefinition?
+    func serviceName(for serviceTypeID: ServiceTypeID?) -> String?
+    
+    func findService(using issuer: String) -> ServiceDefinition?
+    func findServices(byTag searchText: String) -> [ServiceDefinition]
+    func findServicesByTagOrIssuer(
+        _ searchText: String,
+        exactMatch: Bool,
+        useTags: Bool
+    ) -> [ServiceDefinition]
+    func findServices(domain searchText: String) -> [ServiceDefinition]
+    
+    func findLegacyService(using string: String) -> ServiceTypeID?
+    func findLegacyIcon(using string: String) -> IconTypeID?
 }
-// swiftlint:enable all
+
+public final class ServiceDefinitionDatabaseImpl {
+    private let db = ServiceDefinitionDatabaseGenerated()
+    private let legacyExchangeDb = LegacyExchangeDatabase()
+    private let legacyServiceDB: LegacyServiceDatabase = LegacyServiceDatabaseImpl()
+    
+    public init() {}
+}
+
+extension ServiceDefinitionDatabaseImpl: ServiceDefinitionDatabase {
+    public func listAll() -> [ServiceDefinition] {
+        db.services
+    }
+    
+    public func service(using serviceTypeID: ServiceTypeID) -> ServiceDefinition? {
+        db.services.first(where: { $0.serviceTypeID == serviceTypeID })
+    }
+    
+    public func serviceName(for serviceTypeID: ServiceTypeID?) -> String? {
+        guard let serviceTypeID else { return nil }
+        return service(using: serviceTypeID)?.name
+    }
+    
+    public func findLegacyService(using string: String) -> ServiceTypeID? {
+        guard let serviceType = legacyExchangeDb.looselyCheckImportType(for: string) else { return nil }
+        return legacyServiceDB.serviceTypeID(for: serviceType)
+    }
+    
+    public func findLegacyIcon(using string: String) -> IconTypeID? {
+        guard let serviceTypeID = findLegacyService(using: string),
+              let iconTypeID = service(using: serviceTypeID)?.iconTypeID
+        else { return nil }
+        
+        return iconTypeID
+    }
+    
+    public func findService(using issuer: String) -> ServiceDefinition? {
+        // TODO: Duplicated from New Code interactor - remove when migration is properly set up
+        let definitions = listAll()
+        for def in definitions {
+            if let issuerList = def.issuer {
+                for iss in issuerList {
+                    if iss.lowercased() == issuer.lowercased() {
+                        return def
+                    }
+                }
+            }
+            
+            if let issuerRules = def.matchingRules?.filter({ $0.field == .issuer }), !issuerRules.isEmpty {
+                for rule in issuerRules {
+                    if rule.isMatching(for: issuer) {
+                        return def
+                    }
+                }
+            }
+        }
+        return nil
+    }
+    
+    public func findServices(byTag searchText: String) -> [ServiceDefinition] {
+        let query = searchText.uppercased()
+        let definitions = listAll()
+        return definitions.filter({ service in
+            guard let tags = service.tags else { return false }
+            return tags.contains(where: { $0.contains(query) })
+        })
+    }
+    
+    public func findServicesByTagOrIssuer(
+        _ searchText: String,
+        exactMatch: Bool,
+        useTags: Bool
+    ) -> [ServiceDefinition] {
+        let query = searchText.uppercased()
+        let definitions = listAll()
+        return definitions.filter({ service in
+            let name = service.name.uppercased()
+            if exactMatch {
+                if name == query {
+                    return true
+                }
+            } else {
+                if name.contains(query) {
+                    return true
+                }
+            }
+            
+            if let issuerList = service.issuer {
+                for issuer in issuerList {
+                    if exactMatch {
+                        if issuer.uppercased() == query {
+                            return true
+                        }
+                    } else {
+                        if issuer.uppercased().contains(query) {
+                            return true
+                        }
+                    }
+                }
+            }
+            
+            if let issuerRules = service.matchingRules?.filter({ $0.field == .issuer }), !issuerRules.isEmpty {
+                for rule in issuerRules {
+                    if rule.isMatching(for: query) {
+                        return true
+                    }
+                }
+            }
+            
+            if useTags {
+                return service.tags?.contains(where: {
+                    if exactMatch {
+                        return $0.uppercased() == query
+                    } else {
+                        return $0.uppercased().contains(query)
+                    }
+                }) ?? false
+            }
+            return false
+        })
+    }
+    
+    public func findServices(domain searchText: String) -> [ServiceDefinition] {
+        let query = searchText.uppercased()
+        let definitions = listAll()
+        return definitions.filter({ service in
+            query.contains(service.name.uppercased()) ||
+            service.issuer?.first(where: { query.contains($0.uppercased()) }) != nil
+        })
+    }
+}
+
+private final class ServiceDefinitionDatabaseGenerated {
+    var services: [ServiceDefinition] {
+        []
+    }
+}

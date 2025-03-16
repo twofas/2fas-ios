997cc220b5ce046361670c4c7d104bd4bbd466b4
Zbigniew Cisiński <zbigniew@cisinski.com>
2025-03-17 00:13:57 +0100

    #1653 Info record with encryption reference

--- TwoFAS/Sync/Info/InfoHandler.swift
+++ TwoFAS/Sync/Info/InfoHandler.swift
@@ -18,12 +18,20 @@
 //
 
 import Foundation
+import CloudKit
 
 final class InfoHandler {
+    private let allowedDevicesNone = "none"
+    
     private enum Keys: String {
         case metadata
     }
     private let userDefault = UserDefaults()
+    private let zoneID: CKRecordZone.ID
+    
+    init(zoneID: CKRecordZone.ID) {
+        self.zoneID = zoneID
+    }
     
     func infoIfExists() -> [Any] {
         guard metadata() != nil else {
@@ -44,5 +52,34 @@ final class InfoHandler {
     func purge() {
         userDefault.set(nil, forKey: Keys.metadata.rawValue)
         userDefault.synchronize()
+    }
+    
+    func record() -> CKRecord? {
+        if let metadata = metadata() {
+            return InfoRecord.create(
+                with: metadata,
+                version: Info.version,
+                encryption: encryptionType.rawValue,
+                allowedDevices: [allowedDevicesNone],
+                enableWatch: false,
+                encryptionReference: encryptionReference
+            )
+        }
+        return InfoRecord.create(
+            zoneID: zoneID,
+            version: Info.version,
+            encryption: encryptionType.rawValue,
+            allowedDevices: [allowedDevicesNone],
+            enableWatch: false,
+            encryptionReference: encryptionReference
+        )
+    }
+    
+    private var encryptionType: Info.Encryption {
+        return .system
+    }
+    
+    private var encryptionReference: Data {
+        return Data()
     }
 }

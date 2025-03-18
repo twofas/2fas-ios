8d8bb49a8315478c530abb37dfbbed007936e14a
Zbigniew Cisiński <zbigniew@cisinski.com>
2025-03-19 00:12:49 +0100

    #1653 Encryption handling [WIP]

--- TwoFAS/Common/Sources/Extensions/Data+.swift
+++ TwoFAS/Common/Sources/Extensions/Data+.swift
@@ -23,4 +23,20 @@ public extension Data {
     func hexEncodedString() -> String {
         map { String(format: "%02hhx", $0) }.joined()
     }
+    
+    init?(hexString: String) {
+        let len = hexString.count / 2
+        var data = Data(capacity: len)
+        for i in 0 ..< len {
+            let j = hexString.index(hexString.startIndex, offsetBy: i * 2)
+            let k = hexString.index(j, offsetBy: 2)
+            let bytes = hexString[j..<k]
+            if var num = UInt8(bytes, radix: 16) {
+                data.append(&num, count: 1)
+            } else {
+                return nil
+            }
+        }
+        self = data
+    }
 }

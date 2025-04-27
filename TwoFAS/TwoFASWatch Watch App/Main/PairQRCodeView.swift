d34deb63f421d2eb31bc3d3331b15e4a2fb99a65
Zbigniew Cisiński <zbigniew@cisinski.com>
2025-04-27 22:54:00 +0200

    #1653 Watch layout fix

--- TwoFAS/TwoFASWatch Watch App/Main/PairQRCodeView.swift
+++ TwoFAS/TwoFASWatch Watch App/Main/PairQRCodeView.swift
@@ -25,30 +25,30 @@ struct PairQRCodeView: View {
     private let dimension = 120
     private let padding: CGFloat = 24
     var body: some View {
-        if let image = generateQRCode() {
-            ScrollView {
-                VStack {
-                    HStack {
-                        Text("Pair your Apple Watch")
-                            .font(.title2)
-                            .multilineTextAlignment(.leading)
-                        Spacer()
-                    }
+        ScrollView {
+            VStack {
+                HStack {
+                    Text("Pair your Apple Watch")
+                        .font(.title2)
+                        .multilineTextAlignment(.leading)
                     Spacer()
-                    HStack {
-                        Text("Enter Backup section in 2FAS Settings on your iPhone/iPad and navigate to Manage Apple Watches. It will be visible if the state of Cloud Backup is Synced. Scan the QR code from there.")
-                            .font(.body)
-                        Spacer()
-                    }
-                    Spacer(minLength: padding)
+                }
+                Spacer()
+                HStack {
+                    Text("Enter Backup section in 2FAS Settings on your iPhone/iPad and navigate to Manage Apple Watches. It will be visible if the state of Cloud Backup is Synced. Scan the QR code from there.")
+                        .font(.body)
+                    Spacer()
+                }
+                Spacer(minLength: padding)
+                if let image = generateQRCode() {
                     Image(uiImage: image)
                         .interpolation(.none)
                         .renderingMode(.original)
-                    Spacer(minLength: padding)
+                } else {
+                    Text("Error while generating QR Code")
                 }
+                Spacer(minLength: padding)
             }
-        } else {
-            Text("Error while generating QR Code")
         }
     }
     
@@ -62,14 +62,14 @@ struct PairQRCodeView: View {
         )
         generator.withMode(EFQRCodeMode.grayscale)
         generator.withInputCorrectionLevel(EFInputCorrectionLevel.l)
-
+        
         guard let cgImage = generator.generate() else {
             return nil
         }
-
+        
         let image = UIImage(cgImage: cgImage)
             .withRenderingMode(.alwaysOriginal)
-
+        
         return image
     }
 }

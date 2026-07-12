a040ccc22b62ea38d0d042d363c3bf1c86e5ee3f
Zbigniew Cisiński <zbigniew@cisinski.com>
2026-07-12 15:17:05 +0200

    New component for toggle

---
+++ TwoFAS/Common/Sources/UI/TFToggleRow.swift
@@ -0,0 +1,52 @@
+//
+//  This file is part of the 2FAS iOS app (https://github.com/twofas/2fas-ios)
+//  Copyright © 2026 Two Factor Authentication Service, Inc.
+//  Contributed by Zbigniew Cisiński. All rights reserved.
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <https://www.gnu.org/licenses/>
+//
+
+import SwiftUI
+
+/// A capsule-shaped toggle row sitting on `.backgroundsSecondaryElevated`,
+/// used for inline confirmation switches (e.g. "I want to delete this token").
+///
+/// - 16 pt horizontal insets
+/// - Max corner radius from `TFCornerRadius` (`.extraLarge`)
+/// - Uses `AppColor.accentsBrand` as the toggle tint
+public struct TFToggleRow: View {
+    private let title: String
+    @Binding private var isOn: Bool
+
+    public init(_ title: String, isOn: Binding<Bool>) {
+        self.title = title
+        self._isOn = isOn
+    }
+
+    public var body: some View {
+        Toggle(isOn: $isOn) {
+            Text(title)
+                .textStyle(.body)
+                .foregroundStyle(AppColor.labelsPrimary)
+                .multilineTextAlignment(.leading)
+        }
+        .tint(AppColor.accentsBrand)
+        .padding(.horizontal, .XL)
+        .padding(.vertical, .L)
+        .background {
+            RoundedRectangle(.extraLarge)
+                .foregroundStyle(AppColor.backgroundsSecondaryElevated)
+        }
+    }
+}
